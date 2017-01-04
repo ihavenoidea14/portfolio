c27ba1ff8dd8adb31a7e2f2e6c0c6033478192c8
ihavenoidea14 <jarid.wade@icloud.com>
2017-01-03 20:15:28 -0500

    Added favicon

--- app.js
+++ app.js
@@ -12,10 +12,15 @@ var app = express();
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader('X-Powered-By', 'Blood, Sweat, and Tears');
+  next();
+});
+
 
 app.use('/', index);
 
